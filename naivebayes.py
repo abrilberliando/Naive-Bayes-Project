2173ddd417f7e19f7159f97d4589033c2498c290
Abril Berliando <143953111+abrilberliando@users.noreply.github.com>
2024-12-16 00:29:56 +0700

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -165,8 +165,10 @@ def calculate_accuracy(confusion_matrix):
 # Streamlit UI
 st.title("Klasifikasi Kelayakan Nasabah💰")
 
+st.subheader("Download data >>> https://shorturl.at/mhMqk <<<")
+
 # File upload
-uploaded_file = st.file_uploader("Upload data training Nasabah📤 >>> Download >>> https://shorturl.at/mhMqk", type=["xlsx"])
+uploaded_file = st.file_uploader("Upload data training Nasabah📤", type=["xlsx"])
 
 if uploaded_file:
     st.subheader("Data Training Preview👀")
