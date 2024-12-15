96b733bca665e828d6300582c50b61e6e33c641b
Abril Berliando <143953111+abrilberliando@users.noreply.github.com>
2024-12-16 00:37:31 +0700

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -235,4 +235,10 @@ if uploaded_file:
         predicted_class = naive_bayes_predict(sample_to_predict, prior_probs, likelihoods_categorical, likelihoods_numerical, numerical_indices, class_labels)
         st.success(f"Kelas yang Diprediksi: {predicted_class}")
 
+
+st.subtittle(>>> Code By <<<)
+st.write(1. Abril Berliando Cahyariata (2301010186))
+st.write(2. Septaro Travian Gadha (23081010270))
+st.write(3. Alvino Dwi Nengku Wijaya (23081010284))
+
     
