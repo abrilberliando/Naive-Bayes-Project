9dbbd0b19e8df2669f69efadd17566d03e78fe01
Abril Berliando <143953111+abrilberliando@users.noreply.github.com>
2024-12-15 23:43:41 +0700

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import math
+import openpyxl
 
 st.set_page_config(page_title="Klasifikasi Nasabah", page_icon=":money_with_wings:")
 # Fungsi-fungsi utama
