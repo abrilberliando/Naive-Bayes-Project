9b9c9bd73d341886ef28f9e5fc6bc5cb2113777e
Abril Berliando <143953111+abrilberliando@users.noreply.github.com>
2024-12-16 00:41:10 +0700

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -236,9 +236,9 @@ if uploaded_file:
         st.success(f"Kelas yang Diprediksi: {predicted_class}")
 
 
-st.subheader(">>> Code By <<<")
-st.write("1. Abril Berliando Cahyariata (2301010186)")
-st.write("2. Septaro Travian Gadha (23081010270)")
-st.write("3. Alvino Dwi Nengku Wijaya (23081010284)")
+st.subheader(">>> Code By <<<", align="center")
+st.write("1. Abril Berliando Cahyariata (2301010186)", align="center")
+st.write("2. Septaro Travian Gadha (23081010270)", align="center")
+st.write("3. Alvino Dwi Nengku Wijaya (23081010284)", align="center")
 
     
