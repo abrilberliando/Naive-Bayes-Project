8d162ec91e414a3729a216fae5078c1c2e5a12fd
Abril Berliando <143953111+abrilberliando@users.noreply.github.com>
2024-12-15 23:46:01 +0700

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import math
-try:
-    import openpyxl
-except ImportError:
-    raise ImportError("Library 'openpyxl' diperlukan untuk membaca file Excel. Install dengan 'pip install openpyxl'.")
+import openpyxl
+raise ImportError
 
 
 st.set_page_config(page_title="Klasifikasi Nasabah", page_icon=":money_with_wings:")
